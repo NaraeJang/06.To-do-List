1fbbf01054f5ebe5f6bba129949041529ab53999
장나래 <jangnarae@jangnalaes-MacBook-Pro.local>
2023-02-19 16:11:23 -0500

    Replace switch to LocalDateString

--- app.js
+++ app.js
@@ -8,43 +8,51 @@ app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
 
-    switch (currentDay) {
-    case 0:
-        day = "Sunday"
-    break;
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    case 1:
-        day = "Monday"
-    break;
+    var day = today.toLocaleDateString("en-US", options);
+//     var currentDay = today.getDay();
+//     var day = "";
 
-    case 2:
-        day = "Tuesday"
-    break;
+//     switch (currentDay) {
+//     case 0:
+//         day = "Sunday"
+//     break;
 
-    case 3:
-        day = "Wednesday"
-    break;
+//     case 1:
+//         day = "Monday"
+//     break;
 
-    case 4:
-        day = "Thursday"
-    break;
+//     case 2:
+//         day = "Tuesday"
+//     break;
 
-    case 5:
-        day = "Friday"
-    break;
+//     case 3:
+//         day = "Wednesday"
+//     break;
 
-    case 6:
-        day = "Saturday"
-    break;
+//     case 4:
+//         day = "Thursday"
+//     break;
+
+//     case 5:
+//         day = "Friday"
+//     break;
+
+//     case 6:
+//         day = "Saturday"
+//     break;
 
 
-    default:
-        console.log("Error: current day is equal to: " + currentDay);
-    break;
-}
+//     default:
+//         console.log("Error: current day is equal to: " + currentDay);
+//     break;
+// }
 
 res.render("list", {
     KindOfDay: day
