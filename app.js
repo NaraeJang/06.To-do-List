63c7d1b26e9839efcf4706b96f5ea7c793c87a80
장나래 <jangnarae@jangnalaes-MacBook-Pro.local>
2023-02-19 14:38:36 -0500

    Replace to switch and add all days.

--- app.js
+++ app.js
@@ -7,14 +7,49 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
-var today = new Date();
-var currentDay = today.getDay();
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
 
-if (currentDay === 6 || currentDay === 0) {
-    res.sendFile(__dirname + "/index.html");
-} else {
-    res.send("Boo! I have to work!");
+    switch (currentDay) {
+    case 0:
+        day = "Sunday"
+    break;
+
+    case 1:
+        day = "Monday"
+    break;
+
+    case 2:
+        day = "Tuesday"
+    break;
+
+    case 3:
+        day = "Wednesday"
+    break;
+
+    case 4:
+        day = "Thursday"
+    break;
+
+    case 5:
+        day = "Friday"
+    break;
+
+    case 6:
+        day = "Saturday"
+    break;
+
+
+    default:
+        console.log("Error: current day is equal to: " + currentDay);
+    break;
 }
+
+res.render("list", {
+    KindOfDay: day
+});
+
 });
 
 app.listen(3000, function () {
