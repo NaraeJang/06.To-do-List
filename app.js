88916e1eb919e086f92cd8af7275219901e0aa33
NaraeJang <j.naaaarae@gmail.com>
2023-02-20 15:53:53 -0500

    Add Stylesheet

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = [];
+let items = [];
 
 app.set('view engine', 'ejs');
 
@@ -13,16 +13,18 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.get("/", function (req, res) {
-    var today = new Date();
+app.use(express.static("public"));
 
-    var options = {
+app.get("/", function (req, res) {
+    let today = new Date();
+
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {
         KindOfDay: day,
