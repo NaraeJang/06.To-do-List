591e425248f0f1ac9e59055148a8579537344471
NaraeJang <j.naaaarae@gmail.com>
2023-02-20 12:52:15 -0500

    Apply For loop to make ToDo list works

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 //jshint esversion:6
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get("/", function (req, res) {
     var today = new Date();
@@ -16,48 +23,20 @@ app.get("/", function (req, res) {
     };
 
     var day = today.toLocaleDateString("en-US", options);
-//     var currentDay = today.getDay();
-//     var day = "";
 
-//     switch (currentDay) {
-//     case 0:
-//         day = "Sunday"
-//     break;
+    res.render("list", {
+        KindOfDay: day,
+        newListItems: items
+    });
 
-//     case 1:
-//         day = "Monday"
-//     break;
-
-//     case 2:
-//         day = "Tuesday"
-//     break;
-
-//     case 3:
-//         day = "Wednesday"
-//     break;
-
-//     case 4:
-//         day = "Thursday"
-//     break;
-
-//     case 5:
-//         day = "Friday"
-//     break;
-
-//     case 6:
-//         day = "Saturday"
-//     break;
-
-
-//     default:
-//         console.log("Error: current day is equal to: " + currentDay);
-//     break;
-// }
-
-res.render("list", {
-    KindOfDay: day
 });
 
+app.post("/", function (req, res) {
+    item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
 });
 
 app.listen(3000, function () {
